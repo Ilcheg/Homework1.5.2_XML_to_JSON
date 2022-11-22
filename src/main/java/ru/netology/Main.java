ae4154ab855b86a90c676f1e53406940d3a2158f
Ilya Turkovsky <ilcheg@yandex.ru>
2022-11-22 14:49:52 +0300

    Initial commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,5 +1,89 @@
-package ru.netology;public class Main {
+package ru.netology;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Employee> list = parseXML("data.xml");
+        String json = listToJson(list);
+        writeString(json);
+    }
+
+    private static List<Employee> parseXML(String fileName) {
+        List<Employee> list = new ArrayList<>();
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(fileName);
+            doc.getDocumentElement().normalize();
+            NodeList nodeLst = doc.getElementsByTagName("employee");
+
+            for (int i = 0; i < nodeLst.getLength(); i++) {
+                Node fstNode = nodeLst.item(i);
+                if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element fstElmnt = (Element) fstNode;
+                    NodeList fstNmElmntLst = fstElmnt.getElementsByTagName("id");
+                    Element fstNmElmnt = (Element) fstNmElmntLst.item(0);
+                    NodeList fstNm = fstNmElmnt.getChildNodes();
+                    String ID = fstNm.item(0).getNodeValue();
+
+                    NodeList lstNmElmntLst2 = fstElmnt.getElementsByTagName("firstName");
+                    Element lstNmElmnt2 = (Element) lstNmElmntLst2.item(0);
+                    NodeList lstNm2 = lstNmElmnt2.getChildNodes();
+                    String FirstName = lstNm2.item(0).getNodeValue();
+
+                    NodeList lstNmElmntLst3 = fstElmnt.getElementsByTagName("lastName");
+                    Element lstNmElmnt3 = (Element) lstNmElmntLst3.item(0);
+                    NodeList lstNm3 = lstNmElmnt3.getChildNodes();
+                    String LastName = lstNm3.item(0).getNodeValue();
+
+                    NodeList lstNmElmntLst4 = fstElmnt.getElementsByTagName("country");
+                    Element lstNmElmnt4 = (Element) lstNmElmntLst4.item(0);
+                    NodeList lstNm4 = lstNmElmnt4.getChildNodes();
+                    String Country = lstNm4.item(0).getNodeValue();
+
+                    NodeList lstNmElmntLst5 = fstElmnt.getElementsByTagName("age");
+                    Element lstNmElmnt5 = (Element) lstNmElmntLst5.item(0);
+                    NodeList lstNm5 = lstNmElmnt5.getChildNodes();
+                    String Age = lstNm5.item(0).getNodeValue();
+
+                    Employee employee = new Employee(Long.parseLong(ID), FirstName, LastName, Country, Integer.parseInt(Age));
+                    list.add(employee);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private static String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String json = gson.toJson(list, listType);
+        return json;
+    }
+
+    private static void writeString(String json) {
+        try (FileWriter file = new FileWriter("new_Data.json")) {
+            file.write(json);
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
